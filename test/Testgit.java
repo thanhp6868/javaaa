e9a698d465cf4ffb643af39d35942c4990e180bd
thanhp6868 <125439629+thanhp6868@users.noreply.github.com>
2023-09-08 15:43:46 +0700

    update

--- test/Testgit.java
+++ test/Testgit.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class Testgit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+
     }
 }
