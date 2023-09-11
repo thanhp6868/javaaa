bf0987dace20beb05508ed54dbc37610aaa06dc5
thanhp6868 <125439629+thanhp6868@users.noreply.github.com>
2023-09-11 14:16:49 +0700

    Update bai2_DeThi.java

--- test/bai2_DeThi.java
+++ test/bai2_DeThi.java
@@ -1,4 +1,8 @@
+import java.math.*;
+
 public class bai2_DeThi {
+
+    // de 1:
     public class De1_ComplexNumber {
         private double real;
         private double image;
@@ -26,7 +30,60 @@ public class bai2_DeThi {
         }
 
         public double modulus() {
-            return sqrt(this.real * thi.real, this.image * this.image);
+            double modul = (double) this.real * thi.real + (double) this.image * this.image;
+            return sqrt(modul);
         }
+    }
+
+    // de 2
+    public class Point {
+        private double x;
+        private double y;
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Point(Point newPoint) {
+            this.x = newPoint.x;
+            this.y = newPoint.y;
+        }
+
+        public String toString() {
+            return "(" + this.x + "," + this.y + ")";
+        }
+
+        public void move(double dx, double dy) {
+
+        }
+
+        public double distance(Point point) {
+            double distan = (double) point.x * point.x + (double) point.y * point.y;
+            return sqrt(distan);
+        }
+
+    }
+
+    // de 4 - draft
+    public class Battery {
+        private double norminalVoltage;
+        private double capacity;
+        private int chargeCycle;
+
+        public Battery(double volt, double capa, int charg) {
+            this.norminalVoltage = volt;
+            this.capacity = capa;
+            this.chargeCycle = charg;
+        }
+
+        public double norminalEnergy() {
+            return this.norminalVoltage * this.capacity;
+        }
+
+        public double estimate(double dischargePower) {
+            return dischargePower;
+        }
+
     }
 }
