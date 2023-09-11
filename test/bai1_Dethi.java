456e09e02104de9c781492d693e27dfe6b7d527a
thanhp6868 <125439629+thanhp6868@users.noreply.github.com>
2023-09-11 13:50:56 +0700

    Update bai1_Dethi.java

--- test/bai1_Dethi.java
+++ test/bai1_Dethi.java
@@ -5,6 +5,7 @@ public class bai1_Dethi {
         Scanner sc = new Scanner(System.in);
 
         // de1
+        System.out.println("de 1 ");
         int a, b;
 
         System.out.println("Nhap a: ");
@@ -24,6 +25,50 @@ public class bai1_Dethi {
             }
         }
         System.out.println((float) sum / count);
+
+        // de 2
+        System.out.println("de 2 ");
+        System.out.println("Nhap a: ");
+        a = sc.nextInt();
+        System.out.println("Nhap b: ");
+        b = sc.nextInt();
+        sum = 0;
+        for (int i = 0; i < a; i++) {
+            if (i % b != 0)
+                sum += i;
+        }
+        System.out.println(sum);
+
+        // de 4
+        sum = 0;
+        System.out.println("de 4 ");
+        System.out.println("Nhap a: ");
+        a = sc.nextInt();
+        System.out.println("Nhap b: ");
+        b = sc.nextInt();
+        for (int i = a; i <= b; i++) {
+            if (i % 4 == 0 && i % 100 != 0)
+                sum += i;
+        }
+        System.out.println(sum);
+
+        // de 5
+        sum = 0;
+        System.out.println("de 5 ");
+        System.out.println("Nhap so bat ky: ");
+        a = sc.nextInt();
+
+        for (int i = 0; i <= a; i++) {
+            if (i % 7 == 0 && i % 30 != 0)
+                sum += i;
+        }
+        System.out.println(sum);
+
+        // de 6 giong de 1
+
+        // de 7 giong de 4
+
+        // de 8 giong de 2
 
         sc.close();
     }
